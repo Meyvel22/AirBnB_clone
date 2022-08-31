3652bed494becdfbc5eb27468e8f97be50e82d3d
Isaac Takiy <kanytakiy@gmail.com>
2022-08-31 23:40:31 +0000

    Modify base_model.py
    base_model.py has been modified to include the
    ability to create instances of BaseModel with dictionaries

--- models/base_model.py
+++ models/base_model.py
@@ -16,11 +16,25 @@ class BaseModel:
         updated_at(date): represents the last time the object was updated
     """
 
-    def __init__(self):
+    DATE_TIME_FORMAT = '%Y-%m-%dT%H:%M:%S.%f'
+
+    def __init__(self, *args, **kwargs):
         """Contructor of the BaseModel class"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        DATE_TIME_FORMAT = '%Y-%m-%dT%H:%M:%S.%f'
+        if not kwargs:
+            self.id = str(uuid4())
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
+            models.storage.new(self)
+        else:
+            for key, value in kwargs.items():
+                if key in ("updated_at", "created_at"):
+                    self.__dict__[key] = datetime.strptime(
+                        value, DATE_TIME_FORMAT)
+                elif key[0] == "id":
+                    self.__dict__[key] = str(value)
+                else:
+                    self.__dict__[key] = value
 
     def __str__(self):
         """Returns the string representation of the class"""
@@ -43,6 +57,6 @@ class BaseModel:
         properties = set()
         for key, value in self.__dict__.items():
             if key == "created_at" or key == "updated_at":
-                properties[key] = value.isoformat()
+                properties[key] = value.isoformat().strftime(DATE_TIME_FORMAT)
             else:
                 properties[key] = value

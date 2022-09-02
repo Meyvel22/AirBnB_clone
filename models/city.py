2cc93bad1615703a98cbbd019777210a5923c902
Meyvel22 <meyvel22@gmail.com>
2022-09-02 06:34:24 -0700

    Task 8 and 9

---
+++ models/city.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" City class definition that inherits from BaseModel """
+
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """ A class City that inherits from BaseModel """
+
+    state_id = ""
+    name = ""
---
+++ models/state.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" State class definition that inherits from BaseModel """
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """ A class State that inherits from BaseModel """
+
+    name = ""

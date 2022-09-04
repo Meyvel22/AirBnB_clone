d51cd618ad173d005083171467d2a27f39e98017
Meyvel22 <meyvel22@gmail.com>
2022-09-04 08:39:52 -0700

    Fix requ

--- models/base_model.py
+++ models/base_model.py
@@ -33,8 +33,7 @@ class BaseModel:
                         self.__setattr__(key, datetime.fromisoformat(value))
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.created_at = self.updated_at = datetime.now()
             models.storage.new(self)
             models.storage.save()
 

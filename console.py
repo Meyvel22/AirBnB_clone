8838f0574b87644f8530f67469a1f6ccc877b711
Meyvel22 <meyvel22@gmail.com>
2022-09-04 13:36:58 -0700

    console

--- console.py
+++ console.py
@@ -2,6 +2,7 @@
 """ A program that contains the entry point of the command interpreter """
 
 import cmd
+import re
 from shlex import split
 from models.base_model import BaseModel
 from models.user import User
@@ -20,6 +21,7 @@ class HBNBCommand(cmd.Cmd):
 
     model_list = ['BaseModel', 'User', 'State',
                   'City', 'Amenity', "Place", "Review"]
+    queries = ['all', 'count', 'show', 'destroy', 'update']
 
     def precmd(self, line):
         """When precmd() is called, the 'line' is stripped of [, . ()"] then
@@ -45,8 +47,14 @@ class HBNBCommand(cmd.Cmd):
         """Does nothing at an empty line"""
         return False
 
-    def onecmd(self, args):
-        if args == "quit":
+    def onecmd(self, args: str):
+        pattern = re.compile(
+            r"(\w+)\.(\w+)\(((\"[\w|-]+\"),?\s?(\"\w+\")?,?\s?(\"?\w+\"?)?)?\)"
+        )
+        match = re.search(pattern, args)
+        if match:
+            self.handle_match(match)
+        elif args == "quit":
             return self.do_quit(args)
         elif args == "EOF":
             return self.do_EOF(args)
@@ -136,15 +144,15 @@ class HBNBCommand(cmd.Cmd):
         if error:
             return
 
-        args = args.split()
-        key = f"{args[0]}.{args[1]}"
+        args = args.split(" ")
+        objects = models.storage.all()
+        key = ".".join(args)
 
-        objects = storage.all()
-
-        if key in objects and storage.delete(objects[key]):
+        delete = False
+        if key in objects and models.storage.delete(objects[key]):
             pass
         else:
-            print("** instance not found **")
+            print("** no instance found **")
 
     def do_all(self, args):
         """
@@ -219,6 +227,55 @@ class HBNBCommand(cmd.Cmd):
                 return
 
         print("** instance id not found **")
+
+    def handle_match(self, match: re.Match):
+        groups = match.groups()
+        if groups[0] not in HBNBCommand.model_list:
+            print("** class doesn't exist **")
+            return
+        if groups[1] not in HBNBCommand.queries:
+            print(f"** unknown query: '{groups[1]}'")
+            return
+        if groups[1] == 'all':
+            args = f"{groups[1]} {groups[0]}"
+            cmd.Cmd.onecmd(self, args)
+            return
+        elif groups[1] == 'count':
+            args = f"{groups[1]} {groups[0]}"
+            cmd.Cmd.onecmd(self, args)
+            return
+        elif groups[1] == 'show':
+            if groups[3]:
+                id = groups[3][1:-1]
+            else:
+                id =""
+            args = f"{groups[1]} {groups[0]} {id}"
+            cmd.Cmd.onecmd(self, args)
+            return
+        elif groups[1] == "destroy":
+            if groups[3]:
+                id = groups[3][1:-1]
+            else:
+                id =""
+            args = f"{groups[1]} {groups[0]} {id}"
+            cmd.Cmd.onecmd(self, args)
+            return
+        elif groups[1] == 'update':
+            if groups[3] and groups[4]:
+                id = groups[3][1:-1]
+                attr_name = groups[4][1:-1]
+            else:
+                id = ""
+                attr_name = ""
+            if "\"" not in groups[5]:
+                attr_value = groups[5]
+            elif groups[5]:
+                attr_value = groups[5][1:-1]
+            else:
+                attr_value = ""
+            args = f"{groups[1]} {groups[0]} {id} {attr_name} {attr_value}"
+            cmd.Cmd.onecmd(self, args)
+            return
 
 
 if __name__ == "__main__":

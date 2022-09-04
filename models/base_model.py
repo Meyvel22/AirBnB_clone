703317d534353dd692c3db6ed4d66b8606f2c352
Meyvel22 <meyvel22@gmail.com>
2022-09-04 08:48:29 -0700

    BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -40,8 +40,7 @@ class BaseModel:
     def __str__(self):
         """Returns the string representation of the class"""
 
-        return "[{}] ({}) {}". format(self.__class__.__name__, self.id,
-                                      self.__dict__)
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
         """
